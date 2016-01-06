6198bce75fb5e06e06d9ed4daeff0d54819fc4f3
Daniel Lee <lee.daniel84@gmail.com>
2016-01-06 11:03:39 -0700

    IngredientController done

--- IngredientController.swift
+++ IngredientController.swift
@@ -10,22 +10,42 @@ import Foundation
 
 class IngredientController {
     
+    private let ingredientsKey = "ingredients"
+    
     var ingredients: [Ingredient]
     
     
     init() {
         self.ingredients = []
+        self.loadFromPersistentStorage()
     }
     
     func addIngredient(ingredient: Ingredient) {
         ingredients.append(ingredient)
+        self.saveToPersistentStorage()
     }
     
     func removeIngredient(ingredient: Ingredient) {
+        if let ingredientIndex = ingredients.indexOf(ingredient) {
+            ingredients.removeAtIndex(ingredientIndex)
+        }
+        
+        self.saveToPersistentStorage()
+        
     }
     
-    func loadToPersistentStorage() {
+    func loadFromPersistentStorage() {
+        let ingredientDictionariesFromDefaults = NSUserDefaults.standardUserDefaults().objectForKey(ingredientsKey) as? [Dictionary<String, AnyObject>]
         
+        if let ingredientDictionaries = ingredientDictionariesFromDefaults {
+            
+            self.ingredients = ingredientDictionaries.map({Ingredient(dictionary: $0)!})
+        }
+    }
+    
+    func saveToPersistentStorage() {
+        let ingredientDictionaries = self.ingredients.map({$0.dictionaryCopy()})
+        NSUserDefaults.standardUserDefaults().setObject(ingredientDictionaries, forKey: ingredientsKey)
     }
     
     
