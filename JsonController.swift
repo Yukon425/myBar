1c070a6f0fc57617ee3cf1640be77b4be76a700e
Wilks <mikebw7@gmail.com>
2016-01-06 11:16:28 -0700

    first function

---
+++ JsonController.swift
@@ -0,0 +1,40 @@
+//
+//  JsonController.swift
+//  myBar
+//
+//  Created by Michael Wilkowski on 1/6/16.
+//  Copyright © 2016 JustWilks. All rights reserved.
+//
+
+import Foundation
+
+class JsonSerializer {
+    
+    func fetchRecipe(){
+        
+        var recipes: [Recipe] = []
+        
+        guard let json = NSData(contentsOfFile: "/Users/wilks/Projects/fuzzySearch/fuzzySearch/dictionary.json") else {return}
+        
+        let object: AnyObject
+        
+        do {
+            object = try NSJSONSerialization.JSONObjectWithData(json, options: [])
+        } catch {
+            print("Json failed")
+            return
+        }
+        
+        if let dicObject = object as? [String:AnyObject]{
+            let allKeys = dicObject.keys
+            
+            for key in allKeys {
+                guard let recipeDic = dicObject[key] as? [String:AnyObject] else {return}
+                guard let recipeObject = Recipe(dic: recipeDic) else {return}
+                recipes.append(recipeObject)
+            }
+        }
+        
+    }
+    
+}
